b87be5155e6d17ddc1727dd255005bad36c7c36f
xiayirui <ssirius1998@163.com>
2022-05-13 18:11:33 +0800

    feat:canvas2Image

--- src/screenShot/main.js
+++ src/screenShot/main.js
@@ -11,6 +11,11 @@ export default class ScreenShot {
     this.screnShotContainerWidth = window.innerWidth;
     this.screnShotContainerHeight = window.innerHeight;
     this.imageContainer = null;
+
+    this.cutBoxStartX = 0;
+    this.cutBoxStartY = 0;
+    this.cutBoxWidth = this.screnShotContainerWidth;
+    this.cutBoxHeight = this.screnShotContainerHeight;
   }
 
   load() {
@@ -36,6 +41,7 @@ export default class ScreenShot {
         this.cutBoxWidth,
         this.cutBoxHeight,
       );
+      this.addCutBoxEventListener(canvasContainer);
       canvasContainer.appendChild(this.screnShotContainer);
     })
   }
@@ -43,18 +49,22 @@ export default class ScreenShot {
   drawCutBox(
     startX = 0,
     startY = 0,
-    boxWidht = 0,
-    boxHeight = 0,
+    boxWidth,
+    boxHeight,
   ) {
+    // 清除画布
     const context = this.screnShotContainer.getContext('2d');
+    context.clearRect(0, 0, this.screnShotContainerWidth, this.screnShotContainerHeight);
+    // 绘制蒙层
     context.fillStyle = "rgba(0, 0, 0, 0.6)";
     context.fillRect(0, 0, this.screnShotContainerWidth, this.screnShotContainerHeight);
     context.save();
     context.restore();
-    context.clearRect(startX, startY, boxWidht, boxHeight);
+    // 绘制裁剪区
+    context.clearRect(startX, startY, boxWidth, boxHeight);
     context.save();
     context.restore();
-    // 原始图片绘制到canvas的底部
+    // 绘制到canvas画布的底部原始图片
     context.globalCompositeOperation = 'destination-over';
     context.drawImage(
       this.imageContainer,
@@ -65,5 +75,68 @@ export default class ScreenShot {
     )
     context.save();
     context.restore();
+  }
+
+  addCutBoxEventListener(targetDom){
+    // 添加裁剪框的监听
+    targetDom.addEventListener('touchstart', (e) => {
+      const position = targetDom.getBoundingClientRect();
+      this.containerX = position.x;
+      this.containerY = position.y;
+      this.isDragging = true;
+      this.startX = e.touches[0].clientX;
+      this.startY = e.touches[0].clientY;
+      // 原点坐标
+      this.originX = this.cutBoxStartX + this.containerX;
+      this.originY = this.cutBoxStartY + this.containerY;
+      // 右下角坐标
+      this.endX = this.originX + this.cutBoxWidth;
+      this.endY = this.originY + this.cutBoxHeight;
+      this.cutBoxStatus = 'moving';
+    });
+    targetDom.addEventListener('touchmove', (e) => {
+      if(this.isDragging) {
+        console.log('touchmove');
+        const moveX = e.touches[0].clientX - this.startX;
+        const moveY = e.touches[0].clientY - this.startY;
+        switch(this.cutBoxStatus) {
+          case 'moving': 
+            this.tempCutBoxStarX = this.cutBoxStartX + moveX;
+            this.tempCutBoxStarY = this.cutBoxStartY + moveY;
+            break;
+        }
+        this.drawCutBox(
+          this.tempCutBoxStarX,
+          this.tempCutBoxStarY,
+          this.cutBoxWidth,
+          this.cutBoxHeight
+          )
+      }
+    });
+    targetDom.addEventListener('touchend', () => {
+      // 
+      this.cutBoxStartX = this.tempCutBoxStarX;
+      this.cutBoxStartY = this.tempCutBoxStarY;
+    });
+  }
+
+  getImage() {
+    const saveImage = this.screnShotContainer.getContext('2d');
+    const imageData = saveImage.getImageData(
+      this.cutBoxStartX,
+      this.cutBoxStartY,
+      this.cutBoxWidth,
+      this,this.cutBoxHeight,
+    )
+    // 获取图片数据
+    const resCanvas = document.createElement('canvas');
+    resCanvas.height = this.screnShotContainerHeight;
+    resCanvas.width = this.screnShotContainerWidth;
+    const resCanvasContext = this.screnShotContainer.getContext('2d');
+    resCanvasContext.clearRect(0, 0, this.screnShotContainerWidth, this.screnShotContainerHeight);
+    resCanvasContext.putImageData(imageData, 0, 0);
+    resCanvasContext.save();
+    resCanvasContext.restore();
+    return resCanvas.getDataURL();
   }
 }
